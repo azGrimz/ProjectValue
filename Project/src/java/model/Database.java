6b3a6a48d740f20776d160c024d9c19369bfef7a
Victória Cabral Quintério <99900060+raphaelcont@users.noreply.github.com>
2023-06-09 17:10:09 -0300

    Conexão com o MySQL

---
+++ Project/src/java/model/Database.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+     public static Connection getConexao() {
+        Connection con = null;
+        try {
+            String serverName = "localhost";
+            String mydatabase = "projectvalue";
+            String username = "root";
+            String password = "";
+            String driverName = "com.mysql.jdbc.Driver";
+            Class.forName(driverName);
+            String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+            con = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver não encontrado " + e.toString());
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar o Banco de Dados " + e.toString());
+        }
+        return con;
+    }
+}
